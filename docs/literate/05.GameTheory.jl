d500e759541902a9402978210cd623edeb2f53a9
witt3rd <witt3rd@witt3rd.com>
2020-10-15 09:03:08 -0700

    GameTheory and Circuits stubs

---
+++ docs/literate/05.GameTheory.jl
@@ -0,0 +1,18 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 62914ba8-0efe-11eb-0e89-7b5e4021c5e3
+md"# Game Theory"
+
+# ╔═╡ 120d6882-0eff-11eb-272e-597c6181cd1d
+md"## Simulating Monopoly
+
+[Studying Complex Systems - Simulating the Monopoly Board Game](https://www.youtube.com/watch?v=iUmtnActLJo)
+"
+
+# ╔═╡ Cell order:
+# ╟─62914ba8-0efe-11eb-0e89-7b5e4021c5e3
+# ╟─120d6882-0eff-11eb-272e-597c6181cd1d
---
+++ docs/literate/06.Circuits.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8b5ff966-0eff-11eb-11ee-57643e47e347
+md"# Circuits: Hypergraph Categories and Operads
+
+[Applied Category Theory: Chapter 6](https://ocw.mit.edu/courses/mathematics/18-s097-applied-category-theory-january-iap-2019/lecture-videos-and-readings/chapter-6-circuits-hypergraph-categories-and-operads/)
+"
+
+# ╔═╡ Cell order:
+# ╟─8b5ff966-0eff-11eb-11ee-57643e47e347
