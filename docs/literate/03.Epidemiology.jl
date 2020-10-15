0714e820cc46328a3180f8784affe707d282f235
witt3rd <witt3rd@witt3rd.com>
2020-10-15 08:50:29 -0700

    publish epidemiology

--- docs/literate/03.Epidemiology.jl
+++ docs/literate/03.Epidemiology.jl
@@ -58,6 +58,7 @@ begin
 	struct CSet_Petri_Slide end
 	struct Lego_Slide end
 	struct SQL_Slide end
+	struct Ecosystem_Slide end
 	
 	function Base.show(io::IO, ::MIME"image/svg+xml", x::Epidemiological_Cost_Model)
 		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/epidemiology.svg")))
@@ -143,6 +144,10 @@ begin
 		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/sql.png")))
 	end
 
+	function Base.show(io::IO, ::MIME"image/png", x::Ecosystem_Slide)
+		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/ecosystem.png")))
+	end
+
 	nothing
 end
 
@@ -210,12 +215,12 @@ Lego_Slide()
 SQL_Slide()
 
 # ╔═╡ c527f1fa-0efd-11eb-09ba-33706f3951db
-
+Ecosystem_Slide()
 
 # ╔═╡ Cell order:
 # ╟─b164563c-0e54-11eb-0a14-21775ffefaf1
 # ╟─f1652868-0e4f-11eb-09e5-7b23b575cb71
-# ╠═ed5fd302-0ee6-11eb-018d-01d7c068d777
+# ╟─ed5fd302-0ee6-11eb-018d-01d7c068d777
 # ╠═81c0fd9e-0ee9-11eb-3b8e-7d5131b38004
 # ╠═c6ebd4bc-0ef4-11eb-2abf-0730612e5588
 # ╠═cd58373a-0ef4-11eb-2351-752dca43c16b
