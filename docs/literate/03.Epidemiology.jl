a7a77fc36dee43c5cb2ea40744ac8f6ea9d80ec4
witt3rd <witt3rd@witt3rd.com>
2020-10-15 07:46:10 -0700

    add slide images for epidemeology

--- docs/literate/03.Epidemiology.jl
+++ docs/literate/03.Epidemiology.jl
@@ -37,27 +37,38 @@ md"# Epidemiology"
 # ╔═╡ ed5fd302-0ee6-11eb-018d-01d7c068d777
 # Load images
 begin
-	struct A end
-	# struct B end
-	
-	function Base.show(io::IO, ::MIME"image/svg+xml", x::A)
+	struct Epidemiological_Cost_Model end
+    struct Pandemic_Slide end
+	struct City_Spread_Slide end
+
+	function Base.show(io::IO, ::MIME"image/svg+xml", x::Epidemiological_Cost_Model)
 		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/epidemiology.svg")))
 	end
-	# function Base.show(io::IO, ::MIME"image/jpg", x::B)
-	# 	write(io, read(download("https://fonsp.com/img/doggoSmall.jpg?raw=true")))
-	# end
+	
+	function Base.show(io::IO, ::MIME"image/png", x::Pandemic_Slide)
+		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/pandemic.png")))
+	end
+	
+	function Base.show(io::IO, ::MIME"image/png", x::City_Spread_Slide)
+		write(io, read(download("https://raw.githubusercontent.com/mu-ziq/CatML.jl/wip/docs/gh-deploy/dist/images/city_spread.png")))
+	end
+	
 	nothing
 end
 
 # ╔═╡ 81c0fd9e-0ee9-11eb-3b8e-7d5131b38004
-A()
+Epidemiological_Cost_Model()
 
-# ╔═╡ 12518ee2-0eec-11eb-1472-498f5418ad90
+# ╔═╡ c6ebd4bc-0ef4-11eb-2abf-0730612e5588
+Pandemic_Slide()
 
+# ╔═╡ cd58373a-0ef4-11eb-2351-752dca43c16b
+City_Spread_Slide()
 
 # ╔═╡ Cell order:
 # ╟─b164563c-0e54-11eb-0a14-21775ffefaf1
 # ╟─f1652868-0e4f-11eb-09e5-7b23b575cb71
-# ╟─ed5fd302-0ee6-11eb-018d-01d7c068d777
+# ╠═ed5fd302-0ee6-11eb-018d-01d7c068d777
 # ╠═81c0fd9e-0ee9-11eb-3b8e-7d5131b38004
-# ╠═12518ee2-0eec-11eb-1472-498f5418ad90
+# ╠═c6ebd4bc-0ef4-11eb-2abf-0730612e5588
+# ╠═cd58373a-0ef4-11eb-2351-752dca43c16b
