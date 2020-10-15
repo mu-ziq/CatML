65a414659efdf48acdf4c90ecf71b83929955430
witt3rd <witt3rd@witt3rd.com>
2020-10-15 05:44:43 -0700

    initial julia notebook

---
+++ docs/literate/Julia.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cd1b2cfc-0ee3-11eb-377e-7581ddc2366a
+md"# Julia"
+
+# ╔═╡ Cell order:
+# ╠═cd1b2cfc-0ee3-11eb-377e-7581ddc2366a
